e5e6434a8f09a812781bb1444048ddd1df911d02
Guido Perez Ruiz <guidopr27@hotmail.com>
2025-05-17 19:43:02 -0600

    feat: enhance fruit search functionality and update styles for better display

--- client/main.js
+++ client/main.js
@@ -1,18 +1,64 @@
 const $ = (element) => document.querySelector(element)
 
-const fruitInput = $('#search-button')
-console.log(fruitInput)
+const fruitInput = $('#input-text')
+const searchButton = $('#search-button')
+const form = $('#form-content')
+const infoContainer = $('#info-container')
+
+const createCardInfo = (data) => {
+    infoContainer.innerHTML = '';
+    const article = document.createElement('article');
+    article.className = 'nutrition-info-container';
+    article.innerHTML = `
+        <div class="nutrition-info">
+            <header class="nutrition-info-header">
+                <h2>Nutrition Info</h2>
+                <span></span>
+                <h3>${data?.name}</h3>
+                <span></span>
+            </header>
+            <div class="nutrition-info-content">
+                <div class="nutrition-item">
+                    <p>Calories</p>
+                    <p>${data?.nutritions?.calories}</p>
+                </div>
+                <div class="nutrition-item">
+                    <p>Fat</p>
+                    <p>${data?.nutritions?.fat} g</p>
+                </div>
+                <div class="nutrition-item">
+                    <p>Sugar</p>
+                    <p>${data?.nutritions?.sugar} g</p>
+                </div>
+                <div class="nutrition-item">
+                    <p>Carbohydrates</p>
+                    <p>${data?.nutritions?.carbohydrates} g</p>
+                </div>
+                <div class="nutrition-item">
+                    <p>Protein</p>
+                    <p>${data?.nutritions?.protein} g</p>
+                </div>
+            </div>
+        </div>
+    `;
+    infoContainer.appendChild(article)
+}
 
 const getFruitInfo = async fruit => {
-    const url = `https://www.fruityvice.com/api/fruit/${fruit}`
+    const url = `http://localhost:8000/fruit-info/?fruit=${encodeURIComponent(fruit)}`
     try {
         const response = await fetch(url);
         const result = await response.json();
-        console.log(result);
+        createCardInfo(result)
+        infoContainer.style.display = 'block'
     } catch (error) {
-        console.error(error);
+        //Mensaje de error
     }
 
 }
 
-// console.log(getFruitInfo('apple'))
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const fruitText = fruitInput.value
+    getFruitInfo(fruitText)
+})
