76f2406671ff2026928d649f772412fe2fbfbcb2
Guido Perez Ruiz <guidopr27@hotmail.com>
2025-05-17 19:42:24 -0600

    feat: initialize fruit API app with basic structure and functionality

---
+++ server/apps/api/fruit/views.py
@@ -0,0 +1,16 @@
+import requests
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
+# Create your views here.
+@api_view(["GET"])
+def get_fruit_info(request):
+    fruit_name = request.GET.get("fruit", "")
+    try:
+        url = f"https://www.fruityvice.com/api/fruit/{fruit_name}"
+        response = requests.get(url)
+        data = response.json()
+        return Response(data)
+    except Exception as e:
+        return Response({"error": str(e)}, status=500)
